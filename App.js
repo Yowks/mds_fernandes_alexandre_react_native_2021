4193ae98c4bafc9d486b954f8911117879cfa88b
Yowks <48799889+Yowks@users.noreply.github.com>
2021-01-22 00:14:08 +0100

    Final commit

--- App.js
+++ App.js
@@ -1,54 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import NotesScreenComponent from './src/NotesScreenComponent';
-import firebase from 'firebase';
-import LoginScreenComponent from './src/LoginScreenComponent';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+} from "react-native";
+import AppBar from "./src/components/AppBar";
+import Todo from "./src/components/Todo";
+import TodoList from "./src/components/TodoList";
 
 export default function App() {
-  const [userLoggedIn, setUserLoggedIn] = useState(false)
-  
-  if(firebase.apps.length === 0){
-    var firebaseConfig = {
-      apiKey: "AIzaSyAYHHIMmR7MO3uIScUbPsXHTzPY0IYJqg4",
-      authDomain: "todolistrn-d1854.firebaseapp.com",
-      databaseURL: "https://todolistrn-d1854-default-rtdb.europe-west1.firebasedatabase.app/",
-      projectId: "todolistrn-d1854",
-      storageBucket: "todolistrn-d1854.appspot.com",
-      messagingSenderId: "464672197607",
-      appId: "1:464672197607:web:0260ac8d49723122b14b42"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-  }
+  const [title, setTitle] = useState("");
 
-  firebase.auth().onAuthStateChanged((user) => {
-    if(user === null) {
-      setUserLoggedIn(false)
-    } else {
-      setUserLoggedIn(true)
+  // Initalize empty array to store todos
+  const [todos, setTodos] = useState([]);
+
+  const [taskDone, setTaskDone] = useState(0);
+
+  // function to add todo object in todo list
+  const addTodo = () => {
+    if (title.length > 0) {
+      // Add todo to the list
+      setTodos([...todos, { key: Date.now(), name: title, isChecked: false }]);
+      // clear the value of the textfield
+      setTitle("");
     }
-  })
+  };
 
-  if(userLoggedIn) {
-    return (
-      <View style={styles.container}>
-        <NotesScreenComponent/>
-      </View>
+  // function to mark todo as checked or unchecked
+  const checkTodo = (id) => {
+    // loop through todo list and look for the the todo that matches the given id param
+    // update the state using setTodos function
+    setTodos(
+      todos.map((todo) => {
+        if (todo.key === id) {
+          todo.isChecked = !todo.isChecked;
+        }
+        return todo;
+      })
     );
-  } else {
-    return (
-      <View style={styles.container}>
-        <LoginScreenComponent/>
-      </View>
-    );
-  }
+  };
 
+  // function to delete todo from the todo list
+  const deleteTodo = (id) => {
+    // loop through todo list and return todos that don't match the id
+    // update the state using setTodos function
+    setTodos(
+      todos.filter((todo) => {
+        return todo.key !== id;
+      })
+    );
+  };
+
+  useEffect(() => {
+    console.log(todos.length, "TodoList length");
+    //console.log(todos);
+  }, [todos]);
 
   return (
     <View style={styles.container}>
-      {/* <NotesScreenComponent/> */}
-      <LoginScreenComponent/>
+      <View style={styles.todo}>
+        <TextInput
+          placeholder="Add a todo"
+          value={title}
+          onChangeText={(value) => setTitle(value)}
+          style={styles.textbox}
+        />
+        <Button title="Ajouter" color="#7F39FB" onPress={() => addTodo()} />
+      </View>
+
+      <AppBar todos={todos} />
+
+      <ScrollView>
+        {todos.map((todo) => (
+          <TodoList
+            key={todo.key}
+            todo={todo}
+            checkTodo={checkTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -56,8 +90,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  todo: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textbox: {
+    borderWidth: 1,
+    borderColor: "#7F39FB",
+    padding: 10,
+    margin: 10,
+    width: "80%",
   },
 });
